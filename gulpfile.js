d895a2999a6ec58cd30fe7b802b5df0a7419f748
P_Max <pushkarev.max@gmail.com>
2016-01-19 23:28:40 +0400

    upgrade gulp_lite to version 0.2.0;

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,93 @@
 // Load plugins
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    sourcemaps = require('gulp-sourcemaps'),
-    livereload = require('gulp-livereload');
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cssnano = require('gulp-cssnano'),
+	uglify = require('gulp-uglify'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	sourcemaps = require('gulp-sourcemaps'),
+	connect = require('gulp-connect'),
+	layout = require('gulp-nunjucks-render');
 
 // Styles
-gulp.task('styles', function() {
-    return gulp.src('assets/css/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('dev/css'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(minifycss())
-        .pipe(gulp.dest('dev/css'));
+gulp.task('styles', function () {
+	return gulp.src('assets/css/style.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(sourcemaps.write())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dev/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(cssnano())
+		.pipe(gulp.dest('dev/css'))
+		.pipe(connect.reload());
 });
 
 // Scripts
-gulp.task('scripts', function() {
-    return gulp.src(['assets/js/vendor/*.js', 'assets/js/*.js'])
-        .pipe(concat('scripts.js'))
-        .pipe(gulp.dest('dev/js'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify())
-        .pipe(gulp.dest('dev/js'));
+gulp.task('scripts', function () {
+	return gulp.src(['assets/js/vendor/*.js', 'assets/js/*.js'])
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('dev/js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('dev/js'))
+		.pipe(connect.reload());
 });
 
 // Images
-gulp.task('images', function() {
-    return gulp.src('assets/img/**/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('dev/img'));
+gulp.task('images', function () {
+	return gulp.src('assets/img/**/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dev/img'))
+		.pipe(connect.reload());
+});
+
+// Layout
+gulp.task('layout', function () {
+	return gulp.src(['assets/templates/*.html'])
+		.pipe(layout())
+		.pipe(gulp.dest('dev'))
+		.pipe(connect.reload());
+});
+
+// Connect
+gulp.task('connect', function () {
+	connect.server({
+		root: 'dev',
+		livereload: true
+	});
 });
 
 // Default task
-gulp.task('default', function() {
-    gulp.start('styles', 'scripts', 'images');
+gulp.task('build', function () {
+	gulp.start('styles', 'scripts', 'images');
 });
 
 // Watch
-gulp.task('dev', function() {
+gulp.task('watch', function () {
 
-    // Watch .scss files
-    gulp.watch('assets/css/**/*.scss', ['styles']);
+	// Watch .scss files
+	gulp.watch('assets/css/**/*.scss', ['styles']);
 
-    // Watch .js files
-    gulp.watch('assets/js/**/*.js', ['scripts']);
+	// Watch .js files
+	gulp.watch('assets/js/**/*.js', ['scripts']);
 
-    // Watch image files
-    gulp.watch('assets/img/**/*', ['images']);
+	// Watch image files
+	gulp.watch('assets/img/**/*', ['images']);
 
-    // Create LiveReload server
-    livereload.listen();
-
-    // Watch any files in dist/, reload on change
-    gulp.watch(['dev/**']).on('change', livereload.changed);
+	// Watch hmtl files
+	gulp.watch('assets/**/*.html', ['layout']);
 
 });
+
+gulp.task('default', ['connect', 'watch']);
